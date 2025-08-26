c578dbf3da543a8b72d2a47ff60748bc00f4fbac
ryanm09 <ryan.moore@gilmour.org>
2025-08-26 13:15:43 +0000

    tuesday class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,30 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // DATA TYPES
+        int x = 5;
+        char c = 'c';
+        double y = 5.5;
+        boolean b = false;
+        int w; //declaration
+        w = 66; //initialization
+        int infractedStudents; //camelCasing
+        final int CONNY = 5; //constants are all caps
+
+        //OPERATIONS
+        System.out.println(w-3);
+        w -= 3;
+        System.out.println(w);
+        
+
+        //COMPARISONS
+        boolean boo = false;
+        System.out.println(!boo != false);
+
+        //CONDITIONAL - can do on one line without braces
+        if(!boo) System.out.println("boo passed");
+    
+        //SHORT CIRCUIT
+        int divisor = 0;
+        if(divisor != 0 && 5/divisor > 1) System.out.println("omg");
     }
 }
